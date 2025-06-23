f865c49f64f621b62b28eb2a85fd336f6320aa14
berkaykeskin <keskinberkay30@gmail.com>
2025-06-23 14:28:30 +0300

    Navbar is converted into dropdown menus

--- frontend/src/Components/NavBar/NavBar.tsx
+++ frontend/src/Components/NavBar/NavBar.tsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import "./NavBar.css"
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './NavBar.css';
+import { Link } from 'react-router-dom';
 
-type Props = {}
+type Props = {};
 
 const NavBar = (props: Props) => {
   return (
     <div className='nb-container'>
-        <div className='nb-amblem'>
-            <img src="/music-note.png" alt="" style={{width: "40px"}} />
-        </div>
-        <div className='nb-search'>Search</div>
-        <div className='nb-other-pages'>
-            <p>
-                <Link to="/createSong" className="nb-link">Create Song</Link>
-            </p>
-            <p>
-                <Link to="/viewSong" className="nb-link">View Song</Link>
-            </p>
-        </div>
-    </div>
-  )
-}
+      <div className='nb-amblem'>
+        <img src="/music-note.png" alt="logo" style={{ width: "40px" }} />
+      </div>
 
-export default NavBar
+      <div className='nb-search'>Search</div>
+
+      <div className='nb-other-pages'>
+        <div className="nb-dropdown">
+          <span className="nb-dropdown-title">Song</span>
+          <div className="nb-dropdown-content">
+            <Link to="/createSong" className="nb-link">Create Song</Link>
+            <Link to="/viewSong" className="nb-link">View Song</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
